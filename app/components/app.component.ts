5b1b074b595670ee3b5bf6b74e5425b733ac060a
William Duplenne <will.dupl95@gmail.com>
2016-03-21 22:10:58 +0100

    Import Observable

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,5 +1,6 @@
-import {Component, View, EventEmitter} from 'angular2/core';
+import {Component, View} from 'angular2/core';
 import {httpService} from '../services/http.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
     selector: 'my-app'
@@ -10,6 +11,17 @@ import {httpService} from '../services/http.service';
 export class AppComponent {
 	public msg = 'Request result';
 	public datas_error: Boolean = false;
+	public apiKey;
+	public player1;
+	public player2;
+	public player1Friends;
+	public player2Friends;
+	public player1FriendsTotal;
+	public player2FriendsTotal;
+	public player1Games;
+	public player2Games;
+	public player1GamesTotal;
+	public player2GamesTotal;
 
 	constructor(private _httpService: httpService) {
 		this.apiKey = '385FE91CF75CF3037427EBB2CE96D5BF';
@@ -33,7 +45,7 @@ export class AppComponent {
 			data => { 
 				this.player1Friends = data.friendslist.friends;
 				this.player1FriendsTotal = data.friendslist.friends.length;
-				console.log(this.player1Friends);
+				//console.log(this.player1Friends);
 			},
 			err => { this.datas_error = true }
 			);
@@ -41,28 +53,29 @@ export class AppComponent {
 			data => { 
 				this.player2Friends = data.friendslist.friends;
 				this.player2FriendsTotal = data.friendslist.friends.length;
-				console.log(this.player2Friends);
+				//console.log(this.player2Friends);
 			},
 			err => { this.datas_error = true }
 			);
 
 		// Get Owned Games
-		this._httpService.getOwnedGames(first_id, this.apiKey).subscribe(
-			data => { 
-				this.player1Games = data.response.games;
-				this.player1GamesTotal = data.response.game_count;
-				console.log(this.player1Games);
+		Observable.forkJoin(
+			this._httpService.getOwnedGames(first_id, this.apiKey),
+			this._httpService.getOwnedGames(second_id, this.apiKey)
+		).subscribe(
+			data => {
+				this.player1Games = data[0].response.games;
+				this.player2Games = data[1].response.games;
+
+				this.player1GamesTotal = data[0].response.game_count;
+				this.player2GamesTotal = data[1].response.game_count;
+
+				console.log('1', this.player1Games);
+				console.log('2', this.player2Games);
 			},
-			err => { this.datas_error = true }
-			);
-		this._httpService.getOwnedGames(second_id, this.apiKey).subscribe(
-			data => { 
-				this.player2Games = data.response.games;
-				this.player2GamesTotal = data.response.game_count;
-				console.log(this.player2Games);
-			},
-			err => { this.datas_error = true }
-			);
+			err => console.error(err)
+		);
+
 	}
 
 
