46614a98ff12bec5e0dfbd1563dc080b6f4b8323
William Duplenne <will.dupl95@gmail.com>
2016-03-28 12:21:32 +0200

    Update observable

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -22,41 +22,39 @@ export class AppComponent {
 	public player2Games;
 	public player1GamesTotal;
 	public player2GamesTotal;
+	public sameGames = [];
 
 	constructor(private _httpService: httpService) {
 		this.apiKey = '385FE91CF75CF3037427EBB2CE96D5BF';
 	}
 
 	compare(first_id, second_id) {
-		console.log(this);
-
 		// Get Player Summaries
-		this._httpService.getPlayerSummaries(first_id, this.apiKey).subscribe(
-			data => { this.player1 = data.response.players; console.log(this.player1); },
-			err => { this.datas_error = true }
-			);
-		this._httpService.getPlayerSummaries(second_id, this.apiKey).subscribe(
-			data => { this.player2 = data.response.players; console.log(this.player2); },
-			err => { this.datas_error = true }
-			);
+		Observable.forkJoin(
+			this._httpService.getPlayerSummaries(first_id, this.apiKey)
+			this._httpService.getPlayerSummaries(second_id, this.apiKey)
+		).subscribe(
+			data => {
+				this.player1 = data[0].response.players;
+				this.player2 = data[1].response.players;
+			},
+			err => console.error(err)
+		);
 
 		//  Get Frield List
-		this._httpService.getFriendList(first_id, this.apiKey).subscribe(
-			data => { 
-				this.player1Friends = data.friendslist.friends;
-				this.player1FriendsTotal = data.friendslist.friends.length;
-				//console.log(this.player1Friends);
+		Observable.forkJoin(
+			this._httpService.getFriendList(first_id, this.apiKey),
+			this._httpService.getFriendList(second_id, this.apiKey)
+		).subscribe(
+			data => {
+				this.player1Friends = data[0].friendslist.friends;
+				this.player2Friends = data[1].friendslist.friends;
+				
+				this.player1FriendsTotal = this.player1Friends.length;
+				this.player2FriendsTotal = this.player2Friends.length;
 			},
-			err => { this.datas_error = true }
-			);
-		this._httpService.getFriendList(second_id, this.apiKey).subscribe(
-			data => { 
-				this.player2Friends = data.friendslist.friends;
-				this.player2FriendsTotal = data.friendslist.friends.length;
-				//console.log(this.player2Friends);
-			},
-			err => { this.datas_error = true }
-			);
+			err => console.error(err)
+		);
 
 		// Get Owned Games
 		Observable.forkJoin(
@@ -64,14 +62,31 @@ export class AppComponent {
 			this._httpService.getOwnedGames(second_id, this.apiKey)
 		).subscribe(
 			data => {
+				var tabPlayer1 = [];
+				var tabPlayer2 = [];
+				var self = this;
+
 				this.player1Games = data[0].response.games;
 				this.player2Games = data[1].response.games;
 
+				_.each(this.player1Games, function(game) {
+					tabPlayer1.push(game.name);
+				});
+
+				_.each(this.player2Games, function(game) {
+					tabPlayer2.push(game.name);
+				});
+
+				var intersection = _.intersection(tabPlayer1, tabPlayer2);
+
+				_.each(intersection, function(name) {
+					self.sameGames.push(_.find(self.player1Games, function(game) {
+						return game.name === name;
+					}));
+				});
+
 				this.player1GamesTotal = data[0].response.game_count;
 				this.player2GamesTotal = data[1].response.game_count;
-
-				console.log('1', this.player1Games);
-				console.log('2', this.player2Games);
 			},
 			err => console.error(err)
 		);
